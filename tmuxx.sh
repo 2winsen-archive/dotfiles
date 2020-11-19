62008e3421354dd6603e54e3189577ff33cb8468
2winsen <vitalijs.shakels@gmail.com>
2020-11-19 16:31:58 +0200

    shellcheck

--- tmuxx.sh
+++ tmuxx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 tmux new -s 'my-session' -d -n 'home'
-tmux new-window -c '~/workspace' -n 'workspace'
-tmux new-window -c '~/docker' -n 'docker'
+tmux new-window -c "$HOME/workspace" -n 'workspace'
+tmux new-window -c "$HOME/docker" -n 'docker'
 tmux select-window -t 'home'
 tmux -2 attach-session -d
